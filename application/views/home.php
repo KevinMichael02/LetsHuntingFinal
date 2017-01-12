3f2ae537e1400e8a6b7c39e23ce39e530b37aa8c
selviamago <selviamago@hotmail.com>
2017-01-12 18:50:50 +0800

    Update home.php
    update pendaftaran

--- application/views/home.php
+++ application/views/home.php
@@ -51,4 +51,66 @@
     <!-- Services Section -->
     <section id="services">
 
-    </section>
+ <!-- Services Section -->
+    <section id="services">
+
+       
+    <header class="business-header">
+        <div class="container">
+            <div class="row">
+                <div class="row">
+
+                <div class="tap col-sm-6 col-md-5 col-lg-6 isi">
+                <img src=assets/img/lensa.png width="300" height="300" class="d-inline-block align-top" alt="">
+                <h2 class="tagline">How to be a good photographer?</h2>
+                <h2><br>"Memotretlah karena kamu mencintainya, dan karena bagimu hadiah terbesarnya adalah proses memotret itu sendiri"</h2>
+
+                </div>
+
+                <div class="tap col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+                  <form method="post" action="home">
+                  <div class="form-group row">
+                    <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10">
+                      <input class="form-control" type="text" placeholder="Nama Lengkap" id="example-text-input" required name="txt_name">
+                    </div>
+                  </div>
+                  <div class="form-group row">
+                    <label for="example-text-input" class="col-sm-2 col-form-label"></label>
+                    <div class="col-sm-10">
+                      <input class="form-control" type="text" placeholder="NIM" id="example-text-input" required name="txt_nim">
+                    </div>
+                  </div>
+                    <div class="form-group row">
+                      <label for="inputEmail3" class="col-sm-2 col-form-label"></label>
+                      <div class="col-sm-10">
+                        <input type="email" class="form-control" id="inputEmail3" placeholder="Email" required name="txt_emailNew">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="inputPassword3" class="col-sm-2 col-form-label"></label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" id="inputPassword3" placeholder="Password" required name="txt_passwordNew">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="example-tel-input" class="col-sm-2 col-form-label"></label>
+                      <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Phone" id="example-tel-input" required name="txt_phone">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="offset-sm-2 col-sm-10">
+                        <button type="submit" class="btn btn-lg btn-block btn-primary" name='btn_SignUp'>Sign Up!</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                
+                  </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
