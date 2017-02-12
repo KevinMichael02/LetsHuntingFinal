86d5c290a7a30c376ad55ff95a9ba8b19158fc6f
kevyngunanto <kevyngunanto96@gmail.com>
2017-02-12 20:49:11 +0800

    Add files via upload
    update views

---
+++ application/views/infokegiatan.php
@@ -0,0 +1,49 @@
+
+    <br class="featurette-divider">
+  <div class="col-lg-12 text-center">
+      <h1>Kegiatan UKM Fotografi</h1>
+  </div>
+
+    <br class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h1> Hunting Besar </h1>
+          <p class="lead">Hunting Besar, yakni hunting yang diikuti oleh seluruh anggota yang dikoordinir langsung oleh pengurus baik konsep, administrasi dan keuangannya. Dalam Hunting ini kami pengurus UKM mengajak hunting di tempat terbuka dengan menunjukkan bakat anggota/peserta.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/waisak.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+    <br class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h1> Ulang Tahun </h1>
+          <p class="lead">Ulang Tahun Dies Natalis/UKM, dalam rangka memperingati hari lahir kampus STMIK Kharisma/organisasi yang dimeriahkan dengan beberapa kegiatan. Jika ikut dalam partisipasi dalam kegiatan baik dalam foto atau video maka akan mendapatkan point dari bagian bidang Kemahasiswaan dengan memperlihatkan hasil kegiatan yang telah di abadikan. </p>
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" src="img/kathina.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+    <br class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h1> Pelatihan </h1>
+          <p class="lead">Pelatihan/ Workshop, sebuah konsep bidang fotografi yang lebih spesifik dan memerlukan pemahaman yang mendalam dengan berbagai keunikan/kekhasan di dalamnya. <br></p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/asadha.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
+
+    <br class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h1> Pameran </h1>
+          <p class="lead">  Pameran,  suatu konsep fotografi yang dilaksanakan dalam lingkungan eksternalagar bagaimana penikmat pameran lebih dapat ikut merasakan suasana dalam karya yang dipamerkan. </p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" src="img/magha.jpg" alt="Generic placeholder image">
+        </div>
+      </div>
